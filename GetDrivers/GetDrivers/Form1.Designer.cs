2b285e8f4f5f7c080cf9c6d9c5a512c3f34863ae
Arman <armanform@gmail.com>
2013-11-13 17:39:08 +0600

    added open button

--- GetDrivers/GetDrivers/Form1.Designer.cs
+++ GetDrivers/GetDrivers/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.printBtn = new System.Windows.Forms.Button();
+            this.openBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -50,11 +51,22 @@
             this.printBtn.UseVisualStyleBackColor = true;
             this.printBtn.Click += new System.EventHandler(this.printBtn_Click);
             // 
+            // openBtn
+            // 
+            this.openBtn.Location = new System.Drawing.Point(178, 334);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(75, 23);
+            this.openBtn.TabIndex = 2;
+            this.openBtn.Text = "open";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(346, 383);
+            this.Controls.Add(this.openBtn);
             this.Controls.Add(this.printBtn);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -67,6 +79,7 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.Button openBtn;
     }
 }
 
--- GetDrivers/GetDrivers/Form1.cs
+++ GetDrivers/GetDrivers/Form1.cs
@@ -127,5 +127,10 @@ namespace GetDrivers
                 p.WaitForExit();
             }
         }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
